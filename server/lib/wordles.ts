8c87918e0b195c5e5d199f7df632b52a2fc02b7c
Mario Pabon <me@mariopabon.com>
2022-01-27 20:24:43 -0500

    add method for parsing results

---
+++ server/lib/wordles.ts
@@ -0,0 +1,71 @@
+import { User, Wordle, WordleGuessResult, WordleResult } from "@prisma/client";
+import db from "@server/services/db";
+
+export async function getOrCreateWordle(number: number): Promise<Wordle> {
+  const existingWordle = await db.wordle.findFirst({
+    where: {
+      number
+    }
+  })
+  if (existingWordle) {
+    return existingWordle
+  }
+
+  return await db.wordle.create({
+    data: {
+      number,
+      date: new Date()
+    }
+  })
+}
+
+interface ResultData {
+  wordleNumber: number
+  attempts: WordleGuessResult[][]
+}
+
+export async function addResultsForUser(user: User, resultsString: string): Promise<WordleResult> {
+  const resultData = parseResultsFromString(resultsString)
+  const wordle = await getOrCreateWordle(resultData.wordleNumber)
+
+  return await db.wordleResult.create({
+    data: {
+      wordleId: wordle.id,
+      userId: user.id,
+      attempts: {
+        create: resultData.attempts.map(attempt => {
+          return {
+            guesses: attempt
+          }
+        })
+      }
+    }
+  })
+}
+
+function parseResultsFromString(resultsString: string): ResultData {
+  const lines = resultsString.trim().split('\n')
+  const header = lines[0].trim()
+  const attempts = lines.slice(2)
+
+  const headerParts = header.split(' ')
+  const wordleNumber = parseInt(headerParts[1])
+  
+  const parsedAttempts = attempts.map(attempt => {
+    const guesses = Array.from(attempt.trim())
+    return guesses.map(guess => {
+      if (guess == '🟩') {
+        return WordleGuessResult.EXACT_MATCH
+      } else if (guess == '🟨') {
+        return WordleGuessResult.IN_WORD
+      } else {
+        return WordleGuessResult.NOT_IN_WORD
+      }
+    })
+  })
+
+  return {
+    wordleNumber,
+    attempts: parsedAttempts
+  }
+}

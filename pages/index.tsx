316b49ae99a0f8941556a82dc0a2fb823943224c
Mario Pabon <me@mariopabon.com>
2022-01-29 19:23:31 -0500

    allow viewing results without signing in

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,94 +89,93 @@ const Home: NextPage<HomePageProps> = ({
   })
   const results = resultsQuery.data?.results
 
-  if (user) {
-    return (
-      <>
-        <Head>
-          <title>Wordler</title>
-        </Head>
-        <Box mb={3}>
-          <Heading as="h1">Wordler</Heading>
-        </Box>
-        {!hasPostedResultsToday && (
-          <Box mb={5}>
-            <Heading as="h2">post your results</Heading>
-            <form
-              onSubmit={async (event) => {
-                event.preventDefault()
-                const form = event.target as HTMLFormElement
-                const data = new FormData(form)
-                const result = await axios.post('/api/results', {
-                  results: data.get('results'),
-                })
-                form.reset()
-              }}
-            >
-              <textarea
-                name="results"
-                placeholder="Paste results from Wordle here"
-                required
-                style={{
-                  height: 100,
-                  width: 300,
-                }}
-              ></textarea>
-              <br />
-              <button type="submit">Post</button>
-            </form>
-          </Box>
-        )}
+  return (
+    <>
+      <Head>
+        <title>Wordler</title>
+      </Head>
+      <Box mb={3}>
+        <Heading as="h1">Wordler</Heading>
+      </Box>
+      {
+        !user && (
+          <div>
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+            <a href="/api/auth/twitter/authorize">Sign in with Twitter to share and compare your results</a>
+          </div>
+        )
+      }
+      {user && !hasPostedResultsToday && (
         <Box mb={5}>
-          <DatePicker selectedDate={startOfDay(toUTC(new Date(date)))} />
-          {results?.map((result: any) => {
-            return (
-              <Box key={result.id} mb={3}>
-                <WordleResult currentUser={user} result={result} />
-              </Box>
-            )
-          })}
-        </Box>
-        <Box mb={5}>
-          <Heading as="h2">groups</Heading>
-          {groups.length ? (
-            groups.map((group) => {
-              return (
-                <NextLink key={group.id} href={`/groups/${group.slug}`}>
-                  <Link href={`/groups/${group.slug}`}>{group.name}</Link>
-                </NextLink>
-              )
-            })
-          ) : (
-            <Text>Not a member of any groups (yet)</Text>
-          )}
+          <Heading as="h2">post your results</Heading>
           <form
             onSubmit={async (event) => {
               event.preventDefault()
               const form = event.target as HTMLFormElement
               const data = new FormData(form)
-              const result = await axios.post('/api/groups', {
-                name: data.get('name'),
+              const result = await axios.post('/api/results', {
+                results: data.get('results'),
               })
-              router.push(`/groups/${result.data.data.id}`)
+              form.reset()
             }}
           >
-            <label>
-              <span>start a group</span>
-              <br />
-              <input name="name" placeholder="name your group" required />
-            </label>
-            <button type="submit">start</button>
+            <textarea
+              name="results"
+              placeholder="Paste results from Wordle here"
+              required
+              style={{
+                height: 100,
+                width: 300,
+              }}
+            ></textarea>
+            <br />
+            <button type="submit">Post</button>
           </form>
         </Box>
-      </>
-    )
-  }
-
-  return (
-    <div>
-      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-      <a href="/api/auth/twitter/authorize">Sign in with Twitter</a>
-    </div>
+      )}
+      <Box mb={5}>
+        <DatePicker selectedDate={startOfDay(toUTC(new Date(date)))} />
+        {results?.map((result: any) => {
+          return (
+            <Box key={result.id} mb={3}>
+              <WordleResult currentUser={user} result={result} />
+            </Box>
+          )
+        })}
+      </Box>
+      <Box mb={5}>
+        <Heading as="h2">groups</Heading>
+        {groups.length ? (
+          groups.map((group) => {
+            return (
+              <NextLink key={group.id} href={`/groups/${group.slug}`}>
+                <Link href={`/groups/${group.slug}`}>{group.name}</Link>
+              </NextLink>
+            )
+          })
+        ) : (
+          <Text>Not a member of any groups (yet)</Text>
+        )}
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault()
+            const form = event.target as HTMLFormElement
+            const data = new FormData(form)
+            const result = await axios.post('/api/groups', {
+              name: data.get('name'),
+            })
+            router.push(`/groups/${result.data.data.id}`)
+          }}
+        >
+          <label>
+            <span>start a group</span>
+            <br />
+            <input name="name" placeholder="name your group" required />
+          </label>
+          <button type="submit">start</button>
+        </form>
+      </Box>
+    </>
   )
 }
 

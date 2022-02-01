cdc840d0e9d44eee0f6a5e64b0ebf700526f30ba
Mario Pabon <me@mariopabon.com>
2022-01-31 23:07:03 -0500

    add a proper landing page

--- client/views/preview.tsx
+++ client/views/preview.tsx
@@ -1,10 +1,36 @@
 import Link from '@client/components/link'
+import { Box, Button, Heading, Text } from 'theme-ui'
 
 const Preview: React.FC = () => {
   return (
-    <Link href="/api/auth/twitter/authorize" isExternal>
-      Sign in with Twitter
-    </Link>
+    <Box sx={{
+      py: [4, 6]
+    }}>
+      <Heading as="h1" sx={{
+        fontSize: 6,
+        mb: 4
+      }}>
+        Play Wordle with friends.
+      </Heading>
+      { /* @ts-ignore */}
+      <Button as="a" href="/api/auth/twitter/authorize" sx={{
+        fontSize: 3,
+      }}>
+        Sign in with Twitter
+      </Button>
+      <Box mt={6}>
+        <Box mb={3}>
+          <Text>
+            Created by <Link isExternal href="https://twitter.com/realplatanopapi">@realplatanopapi</Link>
+          </Text>
+        </Box>
+        <Box>
+          <Text>
+            <Link isExternal href="https://github.com/realplatanopapi/wordler">View source</Link>
+          </Text>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 

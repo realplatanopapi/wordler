9b56ddfaf311a67fb40c5be20cde83cf171f8f84
Mario Pabon <me@mariopabon.com>
2022-01-31 22:43:55 -0500

    group results by day of week

--- client/views/dashboard/submissions.tsx
+++ client/views/dashboard/submissions.tsx
@@ -1,6 +1,7 @@
 import { User, WordleResult as WordleResultType } from '@client/api'
+import { format, startOfDay } from 'date-fns'
 import React from 'react'
-import { Grid } from 'theme-ui'
+import { Box, Grid, Heading } from 'theme-ui'
 import WordleResult from './wordle-result'
 
 interface Props {
@@ -8,15 +9,51 @@ interface Props {
   results: WordleResultType[]
 }
 
+interface ResultsByDay {
+  [key: string]: WordleResultType[]
+}
+
+const groupResultsByDay = (results: WordleResultType[]): ResultsByDay => {
+  return results.reduce<ResultsByDay>((byDay, result) => {
+    const day = startOfDay(new Date(result.createdAt)).toISOString()
+    const resultsForDay = byDay[day] || []
+
+    return {
+      ...byDay,
+      [day]: resultsForDay.concat(result)
+    }
+  }, {})
+}
+
 const Submissions: React.FC<Props> = ({ currentUser, results }) => {
+  const grouped = groupResultsByDay(results)
+  const today = startOfDay(new Date())
+
   return (
-    <Grid columns={1} gap={2} mx={-4}>
-      {results.map((result) => {
-        return (
-          <WordleResult key={result.id} currentUser={currentUser} result={result} />
-        )
-      })}
-    </Grid>
+    <Box>
+      {
+        Object.keys(grouped).map(day => {
+          const resultsForDay = grouped[day]
+          const dayDate = startOfDay(new Date(day))
+          const isToday = dayDate.valueOf() === today.valueOf()
+
+          return (
+            <Box key={day}>
+              <Heading as="h4" mb={4}>{isToday ? 'Today' : format(dayDate, 'EEEE')}</Heading>
+              <Grid columns={1} gap={2} mx={-4}>
+              {
+                resultsForDay.map(result => {
+                  return (
+                    <WordleResult key={result.id} currentUser={currentUser} result={result} />
+                  ) 
+                })
+              }
+              </Grid>
+            </Box>
+          )
+        })
+      }
+    </Box>
   )
 }
 

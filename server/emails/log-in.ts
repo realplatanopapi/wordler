e666bf36b2673432116558e1ab3d282ea86e20a3
Mario Pabon <me@mariopabon.com>
2022-02-04 09:40:56 -0500

    add a plain text version of email and render link text

--- server/emails/log-in.ts
+++ server/emails/log-in.ts
@@ -1,23 +1,32 @@
 interface Email {
   html: string
+  text: string,
   subject: string
 }
 
-export function buildLoginEmail({
-  loginUrl
-}: {
-  loginUrl: string
-}): Email {
+export function buildLoginEmail({ loginUrl }: { loginUrl: string }): Email {
   const html = `
   <html>
     <body>
-      <a href="${loginUrl}">Click here to log in to your Wordler account</a>
+      <h1>Log in to your Wordler account</h1>
+      <p>
+        <a href="${loginUrl}">Click here to log in to your Wordler account</a>
+      </p>
+      <p>
+        If the above link isn't working, copy and paste this url into your browser:
+      </p>
+      <p>
+        ${loginUrl}
+      </p>
     </body>
   </html>
 `
 
+  const text = `Copy and paste this url into your browser: ${loginUrl}`
+
   return {
     html,
-    subject: 'Log in to your Wordler account'
+    text,
+    subject: 'Log in to your Wordler account',
   }
 }

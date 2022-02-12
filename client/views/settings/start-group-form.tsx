d4d71a8c62286e8d1075e315c38f89378b51a59c
Mario Pabon <me@mariopabon.com>
2022-02-12 13:32:17 -0500

    reset form after starting a group

--- client/views/settings/start-group-form.tsx
+++ client/views/settings/start-group-form.tsx
@@ -2,6 +2,7 @@ import { Group } from '@client/api'
 import ErrorCodeMessage from '@client/components/error-code-message'
 import { getGraphqlErrorCode } from '@client/utils'
 import { useStartGroupMutation } from '@client/__gql__/api'
+import { useState } from 'react'
 import { toast } from 'react-toastify'
 import { Button, Input } from 'theme-ui'
 
@@ -10,10 +11,13 @@ interface Props {
 }
 
 const StartGroupForm: React.FC<Props> = ({ onSubmit }) => {
+  const [groupName, setGroupName] = useState('')
+
   const [startGroup, startGroupResult] = useStartGroupMutation({
     onCompleted: (data) => {
       const { startGroup: group } = data
       if (group) {
+        setGroupName('')
         toast.success('Group started!', {
           toastId: 'groupStarted'
         })
@@ -36,7 +40,9 @@ const StartGroupForm: React.FC<Props> = ({ onSubmit }) => {
       }}
     >
       <ErrorCodeMessage code={getGraphqlErrorCode(startGroupResult.error)} />
-      <Input name="name" placeholder="name your group" required />
+      <Input name="name" placeholder="name your group" value={groupName} onChange={(event) => {
+        setGroupName(event.target.value)
+      }} required />
       <br />
       <Button type="submit">Start</Button>
     </form>

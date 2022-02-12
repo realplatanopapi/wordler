6def32223b52cee4b8e345dbb88024a17522b50b
Mario Pabon <me@mariopabon.com>
2022-02-12 13:14:48 -0500

    show notification when saved

--- client/views/settings/profile.tsx
+++ client/views/settings/profile.tsx
@@ -1,14 +1,24 @@
 import { useUpdateDisplayNameMutation } from "@client/__gql__/api"
 import { UserFromSsr } from "@common/sessions"
 import { useState } from "react"
+import { toast } from "react-toastify"
 import { Box, Button, Heading, Input, Label } from "theme-ui"
+
+import ErrorCodeMessage from "@client/components/error-code-message"
+import { getGraphqlErrorCode } from "@client/utils"
 
 interface Props {
   user: UserFromSsr
 }
 
 const Profile: React.FC<Props> = ({user}) => {
-  const [updateDisplayName, updateDisplayNameResult] = useUpdateDisplayNameMutation()
+  const [updateDisplayName, updateDisplayNameResult] = useUpdateDisplayNameMutation({
+    onCompleted: () => {
+      toast.success('Saved!', {
+        toastId: 'displayNameUpdated'
+      })
+    }
+  })
   const [displayName, setDisplayName] = useState(user.displayName || '')
 
   return (
@@ -16,6 +26,10 @@ const Profile: React.FC<Props> = ({user}) => {
       <Heading mb={4}>Your profile</Heading>
       <form onSubmit={(event) => {
         event.preventDefault()
+
+        if (updateDisplayNameResult.loading) {
+          return
+        }
 
         const form = event.target as HTMLFormElement
         const data = new FormData(form)
@@ -27,10 +41,11 @@ const Profile: React.FC<Props> = ({user}) => {
         })
       }}>
         <Label htmlFor="displayName" mb={3}>Update your username</Label>
+        <ErrorCodeMessage code={getGraphqlErrorCode(updateDisplayNameResult.error)} />
         <Input name="displayName" placeholder="avidWordler45" mb={3} value={displayName} onChange={(event) => {
           setDisplayName(event.target.value)
         }} required />
-        <Button type="submit">Save</Button>
+        <Button type="submit" disabled={updateDisplayNameResult.loading}>Save</Button>
       </form>
     </Box>
   )
